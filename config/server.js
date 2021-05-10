fb363cd201cfa8876e0a3eeacbadd6567a120fc7
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-05-10 20:42:36 -0300

    firts commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'de82f4662af36098872f0e099211c837'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b72df81e-6bd8-424a-a779-af5d0cee1153'
+};
